d5ae49be00923bab56a8b974c808f5163a0a8b00
romar104-hub <m92syn629y@privaterelay.appleid.com>
2026-09-17 16:54:42 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cm-pwa-v1';
+const CACHE='criatorio-marques-v2';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
